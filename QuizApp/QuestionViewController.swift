16a988b5ea1b15c8829de603abba22dbf27b765b
frankm <117271+frankm@users.noreply.github.com>
2020-07-22 22:37:31 -0700

    QuestionVC init() and xib; retrieve answer from selection

---
+++ QuizApp/QuestionViewController.swift
@@ -0,0 +1,58 @@
+//
+//  QuestionViewController.swift
+//  QuizApp
+//
+//  Created by Frank Morales on 7/21/20.
+//  Copyright © 2020 Practice Swift. All rights reserved.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    
+    
+    @IBOutlet weak var headerLabel: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    private var question = ""
+    private var options = [String]()
+    private var selection: ((String) -> Void)?
+    private let reuseIdentifier = "Cell"
+    
+    convenience init(question: String, options: [String], selection: @escaping (String) -> Void ) {
+        self.init()
+        self.question = question
+        self.options = options
+        self.selection = selection
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        headerLabel.text = question
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return options.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = dequeueCell(in: tableView)
+        cell.textLabel?.text = options[indexPath.row]
+        return cell
+    }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selection?(options[indexPath.row])
+    }
+    
+    private func dequeueCell(in tableView: UITableView) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier) {
+            return cell
+        }
+        return UITableViewCell(style: .default, reuseIdentifier: reuseIdentifier)
+    }
+
+}
