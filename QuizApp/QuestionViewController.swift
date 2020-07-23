66cd5a8cbb5b880012fc9531e458c49e6ac0557f
frankm <117271+frankm@users.noreply.github.com>
2020-07-23 01:34:31 -0700

    singleSelection & multiSelection functionality

--- QuizApp/QuestionViewController.swift
+++ QuizApp/QuestionViewController.swift
@@ -17,10 +17,10 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
     
     private var question = ""
     private var options = [String]()
-    private var selection: ((String) -> Void)?
+    private var selection: (([String]) -> Void)?
     private let reuseIdentifier = "Cell"
     
-    convenience init(question: String, options: [String], selection: @escaping (String) -> Void ) {
+    convenience init(question: String, options: [String], selection: @escaping ([String]) -> Void ) {
         self.init()
         self.question = question
         self.options = options
@@ -45,7 +45,18 @@ class QuestionViewController: UIViewController, UITableViewDataSource, UITableVi
     }
 
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        selection?(options[indexPath.row])
+        selection?(selectedOptions(in: tableView))
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        if tableView.allowsMultipleSelection {
+            selection?(selectedOptions(in: tableView))
+        }
+    }
+    
+    private func selectedOptions(in tableView: UITableView) -> [String] {
+        guard let indexPaths = tableView.indexPathsForSelectedRows else { return [] }
+        return indexPaths.map { options[$0.row] }
     }
     
     private func dequeueCell(in tableView: UITableView) -> UITableViewCell {
