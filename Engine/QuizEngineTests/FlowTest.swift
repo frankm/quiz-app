86fb1b87df2436e322b69aba866cb3f315d362e2
frankm <117271+frankm@users.noreply.github.com>
2020-08-06 14:29:34 -0700

    move RouterSpy to private scope; no longer shared

--- Engine/QuizEngineTests/FlowTest.swift
+++ Engine/QuizEngineTests/FlowTest.swift
@@ -135,4 +135,19 @@ class FlowTest: XCTestCase {
         weakSUT = sut
         return sut
     }
+    
+    private class RouterSpy: Router {
+        var routedQuestions = [String]()
+        var routedResult:  Result<String, String>?
+        var answerCallback: (String) -> Void = { _ in }
+
+        func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
+            routedQuestions.append(question)
+            self.answerCallback = answerCallback
+        }
+        
+        func routeTo(result: Result<String, String>) {
+            routedResult = result
+        }
+    }
 }
