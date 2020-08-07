3f2931315a22c2da1d875c6f5eb37e436cc1e2ea
frankm <117271+frankm@users.noreply.github.com>
2020-08-06 19:29:11 -0700

    replace Flow Router with QuizDelegate

--- Engine/QuizEngine/Flow.swift
+++ Engine/QuizEngine/Flow.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-class Flow <R: Router> {
+class Flow <R: QuizDelegate> {
     typealias Question = R.Question
     typealias Answer = R.Answer
 
@@ -30,9 +30,9 @@ class Flow <R: Router> {
     private func routeToQuestion(at index: Int) {
         if index < questions.endIndex {
             let question = questions[index]
-            router.routeTo(question: question, answerCallback: callback(for: question, at: index))
+            router.handle(question: question, answerCallback: callback(for: question, at: index))
         } else {
-            router.routeTo(result: result())
+            router.handle(result: result())
         }
     }
     
